0dffe372de9fb2557c28f927693317ee0a26c833
africamonkey <gzez2012@163.com>
2017-02-23 20:13:33 +0800

    NTT primitive root

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# ACM-Template" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/africamonkey/ACM-Template.git
+git push -u origin master
