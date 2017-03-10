a35bfa5d75ba0d3443428eafb51bbb47b7cc84e1
africamonkey <gzez2012@163.com>
2017-03-10 10:34:39 +0800

    add modifiable-KMP

--- git.sh
+++ git.sh
@@ -4,3 +4,4 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/africamonkey/ACM-Template.git
 git push -u origin master
+
---
+++ upload.sh
@@ -0,0 +1,4 @@
+git add *
+git commit -m "add modifiable-KMP"
+git push -u origin master
+
